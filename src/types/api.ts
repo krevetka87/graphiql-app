98f15e169ea9bffacac9983f7004ff9486e916d8
krevetka87 <bonanast4sia@yandex.ru>
2023-05-03 14:29:29 +0300

    feat: add json parsing error handling

--- src/types/api.ts
+++ src/types/api.ts
@@ -1,6 +1,8 @@
+type TVariables = { [key: string]: string | number };
+
 interface IGraphQLRequest {
   query: string | undefined;
-  variables: { [key: string]: string | number };
+  variables?: TVariables | string;
 }
 
-export type { IGraphQLRequest };
+export type { IGraphQLRequest, TVariables };
--- src/utils/editorHelpers.ts
+++ src/utils/editorHelpers.ts
@@ -2,6 +2,7 @@ import { Uri, editor } from 'monaco-editor/esm/vs/editor/editor.api';
 import { Files } from '../constants/editor';
 import { getQueryResult } from '../api/queryApi';
 import { TEditor, TEditorModel, TEditorOptions } from '../types/editor';
+import { TVariables } from '../types/api';
 
 const getEditorModel = (uri: string, value: string, language: string): TEditorModel => {
   return editor.getModel(Uri.file(uri)) ?? editor.createModel(value, language, Uri.file(uri));
@@ -18,7 +19,16 @@ const createEditor = (
   });
 };
 
-const handleRequest = async () => {
+const parseJSONtoObject = (json: string): TVariables | string => {
+  try {
+    const str: TVariables = JSON.parse(json);
+    return str;
+  } catch (err) {
+    return (err as Error).message;
+  }
+};
+
+const handleRequest = async (): Promise<void> => {
   const queryModelValue: string | undefined = editor
     .getModel(Uri.file(`${Files.query}`))
     ?.getValue();
@@ -29,9 +39,20 @@ const handleRequest = async () => {
 
   const resultModel: TEditorModel | null = editor.getModel(Uri.file(`${Files.result}`));
 
+  let variables: TVariables | string | undefined;
+
+  if (varModelValue) {
+    variables = parseJSONtoObject(varModelValue);
+
+    if (typeof variables === 'string') {
+      resultModel?.setValue(variables);
+      return;
+    }
+  }
+
   const response = await getQueryResult({
     query: queryModelValue,
-    variables: varModelValue ? JSON.parse(varModelValue) : {},
+    variables,
   });
 
   resultModel?.setValue(JSON.stringify(response, null, '\t'));
