ec1c45430913f18b9dbf5028c649e3cb86b59604
Dmitry Ivanov <dimannisik1@gmail.com>
2023-05-14 16:54:41 +0700

    fix: change field validation

--- src/utils/forms.ts
+++ src/utils/forms.ts
@@ -4,22 +4,13 @@ const getFieldsOptions = () => ({
   email: {
     required: i18n.t('forms.errors.email.required'),
     pattern: {
-      value: /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/,
+      value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]{2,4}$/,
       message: i18n.t('forms.errors.email.pattern'),
-    },
-    validate: {
-      letter: (value: string) =>
-        (value && /[A-Za-z]/.test(value)) || i18n.t('forms.errors.email.validation.letter') || '',
-      digit: (value: string) =>
-        (value && /\d/.test(value)) || i18n.t('forms.errors.email.validation.digit') || '',
-      special: (value: string) =>
-        (value && /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/.test(value)) ||
-        i18n.t('forms.errors.email.validation.special') ||
-        '',
     },
   },
   password: {
     required: i18n.t('forms.errors.password.required') || '',
+
     minLength: {
       value: 8,
       message: i18n.t('forms.errors.password.minLength') || '',
@@ -34,6 +25,10 @@ const getFieldsOptions = () => ({
       special: (value: string) =>
         (value && /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/.test(value)) ||
         i18n.t('forms.errors.password.validation.special') ||
+        '',
+      notCyrillic: (value: string) =>
+        (value && /^((?![ЁёА-я]).)*$/.test(value)) ||
+        i18n.t('forms.errors.password.validation.notCyrillic') ||
         '',
     },
   },
